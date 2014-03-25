2ae91e4adfcf1b16936fc36c5dfc2588fb733967
Alex Semin <Alllex.Semin@gmail.com>
2014-03-26 01:00:31 +0400

    ParserBF Unit tests

---
+++ Brainfuck/TM2BF/ASBFi/Test.Parser.fs
@@ -0,0 +1,45 @@
+﻿module Test
+
+open NUnit.Framework
+open ASBi
+
+let ast s = (new ASBi.ParserBF(s)).AST
+
+[<TestFixture>]
+type ``Test compilation``() =
+    [<Test>]
+    member x.``Single characters tests``() =
+        Assert.True(ast "+" = List [Chain ['+']; End])
+        Assert.True(ast "-" = List [Chain ['-']; End])
+        Assert.True(ast "<" = List [Chain ['<']; End])
+        Assert.True(ast ">" = List [Chain ['>']; End])
+        Assert.True(ast "," = List [Chain [',']; End])
+        Assert.True(ast "." = List [Chain ['.']; End])
+        Assert.True(ast "*" = ASBi.ParserBF.EmptyAST)
+        
+    [<Test>]
+    member x.``Chains tests``() =
+        Assert.True(ast ",> ." = List [Chain [','; '>'; '.']; End])
+        Assert.True(ast "+ <-   " = List [Chain ['+'; '<'; '-']; End])
+        Assert.True(ast "+++<<<--->>>,,,...#" = ASBi.ParserBF.EmptyAST)
+        
+    [<Test>]
+    member x.``Loops tests`` () = 
+        Assert.True(ast "+[+]" = List [Chain ['+']; Loop [Chain ['+']]; End])
+        Assert.True(ast "+[<[+]]" = List [Chain ['+']; Loop [Chain ['<']; Loop [Chain ['+']]]; End])
+        Assert.True(ast "+[+]]" = ASBi.ParserBF.EmptyAST)
+        Assert.True(ast "+[[+]" = ASBi.ParserBF.EmptyAST)
+
+    [<Test>]
+    member x.``Addition test``() =
+        Assert.True(ast ",>, [-<+>] <." = List [Chain [','; '>'; ',']; Loop [Chain ['-'; '<'; '+'; '>']]; Chain ['<'; '.']; End])
+
+    [<Test>]
+    member x.``White space testing``() =
+        Assert.True(ast """   
+                             +             
+                             
+                             
+                                    +
+                                    
+                           """ = List [Chain['+'; '+']; End])
